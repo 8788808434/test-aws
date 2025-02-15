6454a56dfcb33820e82bee753dec58f1a71ac371
8788808434 <AP4447628@GMAIL.COM>
2025-02-15 14:55:20 +0530

    test

--- src/main/java/com/example/deployye/controller/TestClass.java
+++ src/main/java/com/example/deployye/controller/TestClass.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestClass {
 
-    @GetMapping
+    @GetMapping("/test")
     public String name(){
         return "Hello Avinash Here";
     }
